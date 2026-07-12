233050943fc9496609737e229e8f64595001598a
m1875 <18754401863@163.com>
2026-07-12 18:51:36 +0800

    feat(common): 通用模块
    1统一返回结果
    2异常类

---
+++ LiveMallBk/livemall-common/src/main/java/com/jolumn/livemallcommon/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.jolumn.livemallcommon.exception;
+
+import com.jolumn.livemallcommon.dto.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(BizException.class)
+    public Result<Void> handleBiz(BizException e) {
+        return Result.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result<Void> handleValidation(MethodArgumentNotValidException e) {
+        String msg = e.getBindingResult().getFieldErrors().stream()
+                .map(f -> f.getDefaultMessage())
+                .findFirst()
+                .orElse("参数错误");
+        return Result.error(400, msg);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result<Void> handleException(Exception e) {
+        log.error("系统异常", e);
+        return Result.error(500, "系统繁忙，请稍后重试");
+    }
+}
