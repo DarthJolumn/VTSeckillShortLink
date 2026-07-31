3e81512a357de0d04ad6c55552fdf7018c6d57c4
m1875 <18754401863@163.com>
2026-07-31 15:52:50 +0800

    fix(seckill): 修正 SeckillMode 注释与实际实现一致

--- vt-seckill-shortlink/vtsl-seckill/src/main/java/com/jolumn/vtslseckill/entity/enums/SeckillMode.java
+++ vt-seckill-shortlink/vtsl-seckill/src/main/java/com/jolumn/vtslseckill/entity/enums/SeckillMode.java
@@ -1,8 +1,7 @@
 package com.jolumn.vtslseckill.entity.enums;
 
 public enum SeckillMode {
-    REDIS_ASYNC,  // Redis Lua + Kafka 异步
-    REDIS_SYNC,   // Redis Lua + Kafka.get() 同步等待
-
-    DB_QUEUE,     // 直接 Kafka 顺序 DB 乐观锁扣减
+    REDIS_ASYNC,  // Redis Lua 原子扣减 + Kafka 异步落库
+    REDIS_SYNC,   // Redis Lua 原子扣减 + Kafka.get() 同步等待落库
+    DB_QUEUE      // DB 乐观锁扣减 + Kafka 顺序消费
 }
