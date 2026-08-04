ffe905e6549c214feaac5bc50c268aa6d50926e6
m1875 <18754401863@163.com>
2026-08-04 18:30:30 +0800

    refactor(shortlink): 重构短链模块

---
+++ vt-seckill-shortlink/vtsl-shortlink/vtsl-shortlink-api/src/main/java/com/jolumn/vtslshortlinkapi/VtslShortlinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.jolumn.vtslshortlinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtslShortlinkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtslShortlinkApiApplication.class, args);
+    }
+
+}
---
+++ vt-seckill-shortlink/vtsl-shortlink/vtsl-shortlink-api/src/test/java/com/jolumn/vtslshortlinkapi/VtslShortlinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolumn.vtslshortlinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtslShortlinkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
